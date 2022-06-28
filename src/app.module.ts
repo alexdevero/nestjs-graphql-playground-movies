42d12f4489a8464da234c3af4cceb60b5e9b6002
Alex Devero <deveroalex@gmail.com>
2022-06-28 16:12:58 +0200

    Update deps

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { join } from 'path';
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       typePaths: ['./**/*.graphql'],
       definitions: {
         path: join(process.cwd(), 'src/graphql.ts'),
